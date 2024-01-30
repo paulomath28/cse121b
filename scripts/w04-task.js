5c3ecb877347d163d40ce40edffe33d7e351e488
paulomath28 <67445880+paulomath28@users.noreply.github.com>
2024-01-30 19:56:36 -0300

    Add files via upload

---
+++ scripts/w04-task.js
@@ -0,0 +1,60 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+
+
+/* Populate Profile Object with placesLive objects */
+
+  
+/* DOM Manipulation - Output */
+
+/* Name */
+
+let myProfile = {};
+myProfile.name = "Paulo André Alves do Nascimento";
+
+/* Photo with attributes */
+
+myProfile.photo = "images/paulo.jpg";
+
+/* Favorite Foods List*/
+
+myProfile.favoriteFoods = [
+    'Lasagna',
+    'Strogonoff',
+    'Pizza',
+    'Breaded steak with fries',
+    'Chocolate trifle',
+    'Banoffe',
+    'Ice cream'
+  ];
+
+/* Hobbies List */
+
+myProfile.hobbies = [
+    'Watch series and films',
+    'Play soccer',
+    'Play the piano',
+    'Listen to music',
+    'To read'
+  ];
+
+
+/* Places Lived DataList */
+
+myProfile.placesLived = [];
+
+myProfile.placesLived.push(
+    {
+      place: 'Rio de Janeiro, RJ',
+      length: '41 years'
+      
+    }
+  );
+
+  myProfile.placesLived.push({
+    place: 'Florianopolis, SC',
+    length: '2 years'
+  });
+  
+    
